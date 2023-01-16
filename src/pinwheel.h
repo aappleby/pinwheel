4e7937fb4aa4da4dc6abbf69765690b1b16683bb
Austin Appleby <aappleby@gmail.com>
2023-01-15 22:25:46 -0800

    checkpoint

--- src/pinwheel.h
+++ src/pinwheel.h
@@ -5,7 +5,6 @@
 // metron_noconvert
 #include "console.h"
 #include "constants.h"
-#include "regfile.h"
 #include "pinwheel_core.h"
 
 //------------------------------------------------------------------------------
@@ -40,32 +39,22 @@ public:
   bool load_elf(const char* firmware_filename);
 
   //----------------------------------------
+
+  logic<32> get_debug() const {
+    return debug_reg;
+  }
+
+  //----------------------------------------
   // FIXME const local variable should not become parameter
 
   void tock(logic<1> reset_in) {
 
-    logic<32> rs1_b  = b5(core.insn_b, 15) ? core.regs.get_rs1() : b32(0);
-    logic<32> rs2_b  = b5(core.insn_b, 20) ? core.regs.get_rs2() : b32(0);
-    logic<32> imm_b  = core.decode_imm(core.insn_b);
-    logic<32> addr_b = b32(rs1_b + imm_b);
 
     //----------
     // Memory
 
-    logic<1> console1_cs_b = b4(core.addr_b(), 28) == 0x4;
-    logic<1> console2_cs_b = b4(core.addr_b(), 28) == 0x5;
-    logic<1> console3_cs_b = b4(core.addr_b(), 28) == 0x6;
-    logic<1> console4_cs_b = b4(core.addr_b(), 28) == 0x7;
-    logic<1> debug_cs_b    = b4(core.addr_b(), 28) == 0xF;
-
-    logic<1> code_cs_c     = b4(core.addr_c, 28) == 0x0;
-    logic<1> console1_cs_c = b4(core.addr_c, 28) == 0x4;
-    logic<1> console2_cs_c = b4(core.addr_c, 28) == 0x5;
-    logic<1> console3_cs_c = b4(core.addr_c, 28) == 0x6;
-    logic<1> console4_cs_c = b4(core.addr_c, 28) == 0x7;
     logic<1> data_cs_c     = b4(core.addr_c, 28) == 0x8;
     logic<1> debug_cs_c    = b4(core.addr_c, 28) == 0xF;
-    logic<1> regfile_cs_c  = b4(core.addr_c, 28) == 0xE;
 
     logic<32> data_out_c = 0;
     if (data_cs_c) {
@@ -74,19 +63,21 @@ public:
     else if (debug_cs_c) {
       data_out_c = debug_reg;
     }
-    else if (regfile_cs_c) {
-      data_out_c = core.regs.get_rs1();
-    }
 
     core.tock(reset_in, code.rdata(), data_out_c);
 
-    next_debug_reg = (b5(core.insn_b, 2) == RV32I::OP_STORE) && debug_cs_b ? rs2_b : debug_reg;
+    logic<1> code_cs_b     = b4(core.addr_b(), 28) == 0x0;
+    logic<1> console1_cs_b = b4(core.addr_b(), 28) == 0x4;
+    logic<1> console2_cs_b = b4(core.addr_b(), 28) == 0x5;
+    logic<1> console3_cs_b = b4(core.addr_b(), 28) == 0x6;
+    logic<1> console4_cs_b = b4(core.addr_b(), 28) == 0x7;
+    logic<1> data_cs_b     = b4(core.addr_b(), 28) == 0x8;
 
     //----------
     // Submod tocks
 
-    code.tock    (b12(core.code_addr), core.code_wdata, core.code_wmask, core.code_wren);
-    data_ram.tock(b12(core.bus_addr), core.bus_wdata, core.bus_wmask, core.bus_wren);
+    code.tock    (b12(core.code_addr), core.code_wdata, core.code_wmask, core.code_wren && code_cs_b);
+    data_ram.tock(b12(core.bus_addr),  core.bus_wdata,  core.bus_wmask,  core.bus_wren  && data_cs_b);
 
     // metron_noconvert
     console1.tock(console1_cs_b && b5(core.insn_b, 2) == RV32I::OP_STORE, core.bus_wdata);
@@ -99,24 +90,20 @@ public:
   }
 
   //----------------------------------------
-
-  logic<32> get_debug() const {
-    return debug_reg;
-  }
-
-  //----------------------------------------
   // FIXME trace modules individually
 
   void tick(logic<1> reset_in) {
-    core.tick(reset_in);
+
 
     if (reset_in) {
       debug_reg = 0;
     }
     else {
-      debug_reg = next_debug_reg;
+      logic<1> debug_cs_b  = b4(core.addr_b(), 28) == 0xF;
+      if (core.bus_wren && debug_cs_b) debug_reg = core.bus_wdata;
     }
 
+    core.tick(reset_in);
     code.tick();
     data_ram.tick();
     core.regs.tick();
@@ -140,7 +127,6 @@ public:
 
   // metron_internal
   pinwheel_core core;
-  logic<32> next_debug_reg;
   logic<32> debug_reg;
   logic<32> gpio_dir;
   logic<32> gpio_in;
