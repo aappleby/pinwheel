8041af8ec8055663d271c48d7fc070e3a8c98840
Austin Appleby <aappleby@gmail.com>
2024-05-29 19:16:19 -0700

    submodules, how do they work

---
+++ top.v
@@ -0,0 +1,17 @@
+`default_nettype none
+
+module pinwheel_ice40(
+  input  logic EXT_CLK,
+  output logic [7:0] LEDS,
+);
+
+  assign LEDS[0] = 1'b0;
+  assign LEDS[1] = 1'b0;
+  assign LEDS[2] = 1'b1;
+  assign LEDS[3] = 1'b1;
+  assign LEDS[4] = 1'b0;
+  assign LEDS[5] = 1'b1;
+  assign LEDS[6] = 1'b1;
+  assign LEDS[7] = 1'b0;
+
+endmodule
