3aa41533ea654cc154b2cba8feb72b7158304865
Austin Appleby <aappleby@gmail.com>
2024-07-31 02:18:44 -0700

    checkpoint

--- uart/bench.v
+++ uart/bench.v
@@ -7,9 +7,10 @@ module simple_message
 (
   input  logic      _clock,
   input  logic      _reset,
+
   output logic[7:0] _out,
-  output logic      _valid,
-  input  logic      _ready
+  output logic      _out_valid,
+  input  logic      _out_ready
 );
 
   localparam delay_max = 20;
@@ -31,7 +32,7 @@ module simple_message
     end else if (delay) begin
       delay_  = delay - 1;
       cursor_ = cursor;
-    end else if (_valid && _ready) begin
+    end else if (_out_valid && _out_ready) begin
       delay_  = cursor == 5 ? delay_max - 1 : delay;
       cursor_ = cursor == 5 ? 0 : cursor + 1;
     end else begin
@@ -45,8 +46,8 @@ module simple_message
     cursor <= cursor_;
   end
 
-  assign _out   = _reset ? 0 : delay ? 0 : text[cursor];
-  assign _valid = _reset ? 0 : (delay == 0);
+  assign _out       = _reset ? 0 : delay ? 0 : text[cursor];
+  assign _out_valid = _reset ? 0 : (delay == 0);
 
 endmodule
 
@@ -60,11 +61,11 @@ module simple_tx
   input logic      _clock,
   input logic      _reset,
 
-  input logic[7:0] _tx_in,
-  input logic      _source_valid,
+  input logic[7:0] _in,
+  input logic      _in_valid,
+  output logic     _in_ready,
 
-  output logic     _sink_ready,
-  output logic     _tx_out
+  output logic     _out
 );
 
   localparam bits_per_byte = 10; // 1 start + 8 data + 1 stop
@@ -72,47 +73,37 @@ module simple_tx
   localparam delay_max = clocks_per_bit - 1;
   localparam count_max = bits_per_byte - 1; // 1 start + 8 data + 9 stop to make sure we're really stopped :D
 
-  logic[7:0] tx_delay, tx_delay_;
-  logic[7:0] tx_count, tx_count_;
-  logic[9:0] tx_shift, tx_shift_;
+  logic[7:0] tx_delay_, tx_delay = delay_max;
+  logic[7:0] tx_count_, tx_count = count_max;
+  logic[9:0] tx_shift_, tx_shift = 10'b1111111111;
 
-  initial begin
-    tx_delay = delay_max;
-    tx_count = count_max;
-    tx_shift = 10'b1111111111;
-  end
-
-  assign _sink_ready = !_reset && tx_delay == delay_max && tx_count == count_max;
-  assign _tx_out   = tx_shift[0];
-
-  wire _tx_bit   = tx_delay == 0 && tx_count > 0 && tx_count < count_max;
-  wire _tx_byte  = tx_delay == 0 && tx_count == 1;
 
   always_comb begin
+    _out = tx_shift[0];
+
     if (_reset) begin
       tx_delay_ = delay_max;
       tx_count_ = count_max;
-    end else if (tx_delay < delay_max) begin
-      tx_delay_ = tx_delay + 1;
-      tx_count_ = tx_count;
-    end else if (tx_count < count_max) begin
-      tx_delay_ = 0;
-      tx_count_ = tx_count + 1;
-    end else if (_source_valid && _sink_ready) begin
-      tx_delay_ = 0;
-      tx_count_ = 0;
-    end
-  end
-
-  always_comb begin
-    if (_reset) begin
       tx_shift_ = 10'b1111111111;
-    end else if (_source_valid && _sink_ready) begin
-      tx_shift_ = { 1'b1, _tx_in, 1'b0 };
-    end else if (tx_delay_ == 0) begin
-      tx_shift_ = { 1'b1, tx_shift[9:1] };
+      _in_ready = 0;
     end else begin
       tx_shift_ = tx_shift;
+      _in_ready = 0;
+      if (tx_delay < delay_max) begin
+        tx_delay_ = tx_delay + 1;
+        tx_count_ = tx_count;
+      end else if (tx_count < count_max) begin
+        tx_delay_ = 0;
+        tx_count_ = tx_count + 1;
+        tx_shift_ = { 1'b1, tx_shift[9:1] };
+      end else begin
+        _in_ready = 1;
+        if (_in_valid) begin
+          tx_delay_ = 0;
+          tx_count_ = 0;
+          tx_shift_ = { 1'b1, _in, 1'b0 };
+        end
+      end
     end
   end
 
@@ -137,8 +128,8 @@ module simple_rx
   input logic       _in,
 
   output logic[7:0] _out,
-  output logic      _source_valid,
-  input  logic      _sink_ready
+  output logic      _out_valid,
+  input  logic      _out_ready
 );
 
   localparam bits_per_byte = 10; // 1 start + 8 data + 1 stop
@@ -146,85 +137,68 @@ module simple_rx
   localparam delay_max = clocks_per_bit - 1;
   localparam count_max = bits_per_byte - 1; // 1 start + 8 data + 9 stop to make sure we're really stopped :D
 
-  logic[7:0] rx_delay, rx_delay_;
-  logic[7:0] rx_count, rx_count_;
-
-  logic[7:0] rx_shift, rx_shift_;
-  logic[7:0] rx_data,  rx_data_;
-  logic[7:0] rx_valid, rx_valid_;
-
-  logic[31:0] rx_sum, rx_sum_;
-
-  initial begin
-    rx_delay = delay_max;
-    rx_count = count_max;
-    rx_shift = 0;
-    rx_data  = 0;
-    rx_sum   = 0;
-  end
-
-  assign _source_valid = _reset ? 0 : rx_delay == delay_mid && rx_count == count_max - 1;
-  assign _out   = rx_data;
+  logic[7:0] delay_, delay = delay_max;
+  logic[7:0] count_, count = count_max;
+  logic[7:0] shift_, shift = 0;
 
   //----------------------------------------
 
-  always_comb begin
-    if (_reset) begin
-      rx_delay_ = delay_max;
-      rx_count_ = count_max;
-    end else if (rx_delay < delay_max) begin
-      rx_delay_ = rx_delay + 1;
-    end else if (rx_count < count_max) begin
-      rx_delay_ = 0;
-      rx_count_ = rx_count + 1;
-    end else if (_in == 0) begin
-      rx_delay_ = 0;
-      rx_count_ = 0;
-    end else begin
-      rx_delay_ = rx_delay;
-      rx_count_ = rx_count;
-    end
-  end
+  /*
+  if delay < delay_max
+    @delay = delay + 1
+  else if count < count_max
+    @delay = 0
+    @count = count + 1
+  else if ser_in == 0
+    @delay = 0
+    @count = 0
 
-  //----------------------------------------
+  if delay == delay_mid
+    @shift = (ser_in :: shift) >> 1
+    valid = count == 8
+  */
 
   always_comb begin
-    if (_reset) begin
-      rx_data_  = 0;
-      rx_valid_ = 0;
-    end else if (rx_delay_ == delay_mid && rx_count_ == count_max - 1) begin
-      rx_data_  = rx_shift_;
-      rx_sum_   = rx_sum + rx_shift_;
-      rx_valid_ = 1;
-    end else begin
-      rx_data_  = rx_data;
-      rx_valid_ = rx_valid;
-    end
-  end
+    delay_ = delay;
+    count_ = count;
+    shift_ = shift;
+    _out = 0;
+    _out_valid = 0;
 
-  //----------------------------------------
-
-  always_comb begin
     if (_reset) begin
-      rx_shift_ = 0;
-    end else if (rx_delay_ == delay_mid) begin
-      rx_shift_ = {_in, rx_shift[7:1]};
+      delay_ = delay_max;
+      count_ = count_max;
+      shift_ = 0;
     end else begin
-      rx_shift_ = rx_shift;
+      if (delay < delay_max) begin
+        delay_ = delay + 1;
+      end else if (count < count_max) begin
+        delay_ = 0;
+        count_ = count + 1;
+      end else if (_in == 0) begin
+        delay_ = 0;
+        count_ = 0;
+      end
+
+      if (delay_ == delay_mid) begin
+        shift_ = {_in, shift[7:1]};
+        if (count_ == count_max - 1) begin
+          _out       = shift_;
+          _out_valid = 1;
+        end
+      end
     end
   end
 
   //----------------------------------------
 
   always @(posedge _clock) begin
-    rx_data  <= rx_data_;
-    rx_delay <= rx_delay_;
-    rx_count <= rx_count_;
-    rx_shift <= rx_shift_;
-    rx_sum   <= rx_sum_;
+    delay <= delay_;
+    count <= count_;
+    shift <= shift_;
   end
 
-  wire _rx_bit = rx_delay == delay_mid && rx_count > 0 && rx_count < count_max;
+  wire _rx_bit = delay == delay_mid && count > 0 && count < count_max;
 
 endmodule
 
@@ -234,13 +208,22 @@ module byte_sink
 (
   input logic      _clock,
   input logic      _reset,
+
   input logic[7:0] _data,
-  input logic      _source_valid,
-  output logic     _sink_ready
+  input logic      _data_valid,
+  output logic     _data_ready
 );
 
+  logic[7:0] data = 0;
+
   always_comb begin
-    _sink_ready = !_reset;
+    _data_ready = !_reset;
+  end
+
+  always @(posedge _clock) begin
+    if (_data_valid) begin
+      data <= _data;
+    end
   end
 
 endmodule
@@ -268,31 +251,32 @@ module bench();
 
   localparam clocks_per_bit = 1;
 
-  simple_message msg();
+  simple_message msg(
+    ._clock     (clock),
+    ._reset     (reset),
+    ._out_ready (tx._in_ready)
+  );
 
-  assign msg._clock = clock;
-  assign msg._reset = reset;
-  assign msg._ready = tx._sink_ready;
+  simple_tx #(.clocks_per_bit(clocks_per_bit)) tx(
+    ._clock    (clock),
+    ._reset    (reset),
+    ._in       (msg._out),
+    ._in_valid (msg._out_valid)
+  );
 
-  simple_tx #(.clocks_per_bit(clocks_per_bit)) tx();
+  simple_rx #(.clocks_per_bit(clocks_per_bit)) rx(
+    ._clock     (clock),
+    ._reset     (reset),
+    ._in        (tx._out),
+    ._out_ready (sink._data_ready)
+  );
 
-  assign tx._clock    = clock;
-  assign tx._reset    = reset;
-  assign tx._tx_in    = msg._out;
-  assign tx._source_valid = msg._valid;
-
-  simple_rx #(.clocks_per_bit(clocks_per_bit)) rx();
-
-  assign rx._clock = clock;
-  assign rx._reset = reset;
-  assign rx._in = tx._tx_out;
-  assign rx._sink_ready = sink._sink_ready;
-
-  byte_sink sink();
-  assign sink._clock = clock;
-  assign sink._reset = reset;
-  assign sink._data  = rx._out;
-  assign sink._source_valid = rx._source_valid;
+  byte_sink sink(
+    ._clock      (clock),
+    ._reset      (reset),
+    ._data       (rx._out),
+    ._data_valid (rx._out_valid)
+  );
 
   //----------------------------------------------------------------------------
 
--- uart/simple_rx.mt
+++ uart/simple_rx.mt
@@ -1,19 +1,19 @@
 // Simple 8n1 UART receiver in Metron
 
 param clocks_per_bit = 4
-param bits_per_byte  = 10
 
-param delay_mid = clocks_per_bit / 2
-param delay_max = clocks_per_bit - 1
-param count_max = bits_per_byte - 1
+const bits_per_byte = 10
+const delay_mid = clocks_per_bit / 2
+const delay_max = clocks_per_bit - 1
+const count_max = bits_per_byte - 1
 
-ser_in : sig[1]  = input
-delay  : reg[8]  = delay_max
-count  : reg[8]  = count_max
-shift  : reg[8]  = 0
-out    : sig[8]  = @shift
-valid  : sig[1]  = 0
-
+$ser_in    : u1  = input
+@delay     : u8  = delay_max
+@count     : u8  = count_max
+@shift     : u8  = 0
+$out       : u8  = @shift
+$out_valid : u1  = 0
+$out_ready : u1  = 0
 
 if delay < delay_max
   @delay = delay + 1
@@ -25,5 +25,5 @@ else if ser_in == 0
   @count = 0
 
 if delay == delay_mid
-  @shift = (ser_in :: shift) >> 1
-  valid = count == 8
+  @shift = ($ser_in :: shift) >> 1
+  $out_valid = count == 8
